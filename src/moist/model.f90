fc098cbf111b7bdc914c56ff5ee5d82fd158fc85
Lukas Wittmann <mail@lukaswittmann.com>
2026-08-10 08:35:26 +0200

    Added general, list-based solvation model

--- src/moist/model.f90
+++ src/moist/model.f90
@@ -1,9 +1,11 @@
 !> Re-export of all solvation models
 module moist_model
    use moist_type, only: solvation_model
+   use moist_model_general, only: general_solvation_model, new_general_model
    implicit none
    private
 
    public :: solvation_model
+   public :: general_solvation_model, new_general_model
 
 end module moist_model
