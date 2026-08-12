585c58f885d831dde93bda814eabba4c9b207fff
Lukas Wittmann <mail@lukaswittmann.com>
2026-08-12 08:19:16 +0200

    Added cosmo reexport

--- src/moist/model/component.f90
+++ src/moist/model/component.f90
@@ -1,13 +1,14 @@
 module moist_model_components
 
    use moist_model_component_gostshyp, only: gostshyp, new_gostshyp
-   use moist_model_component_pcm, only: cpcm, new_cpcm, solver_type, potential_source
+   use moist_model_component_pcm, only: cpcm, new_cpcm, cosmo, new_cosmo, &
+      solver_type, potential_source
    use moist_model_component_pv, only: pv, new_pv
 
    implicit none
    private
 
-   public :: cpcm, new_cpcm, solver_type, potential_source
+   public :: cpcm, new_cpcm, cosmo, new_cosmo, solver_type, potential_source
    public :: gostshyp, new_gostshyp
    public :: pv, new_pv
 
